2de455d03b54d4ec1ca381e506bb5f9a3655415d
Matthew Guizar <matthewrguizar@gmail.com>
2022-08-29 08:10:44 -0700

    removing locks by adding atomic integer

--- Main.java
+++ Main.java
@@ -2,19 +2,22 @@ import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class Main {
     
-    static int counter = 0;
+    //static int counter = 0;
+    static AtomicInteger counter = new AtomicInteger(0);
+    
     
     public static void main(String[] args) {
         CountDownLatch latch = new CountDownLatch(2);
-        ReentrantLock lock = new ReentrantLock();
+        // ReentrantLock lock = new ReentrantLock();
         int nThreads = Runtime.getRuntime().availableProcessors();
         ExecutorService executor = Executors.newFixedThreadPool(nThreads);
         for (int i = 0; i < 2; i++) {
-            executor.submit(() -> task(lock, latch));
+            executor.submit(() -> task(latch));
             
         }
         
@@ -32,11 +35,11 @@ public class Main {
     
     }
     
-    public static void task(ReentrantLock lock, CountDownLatch latch) {
+    public static void task(CountDownLatch latch) {
         for (int i = 0; i < 10000; i++) {
-            lock.lock();
-            counter++; 
-            lock.unlock();
+            // lock.lock();
+            counter.addAndGet(1); //atomic allows the removal of locks
+            // lock.unlock();
         }
         latch.countDown();
     }
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 Eliminating Race Conditions(Where memory is accessed by two or more threads). Updating the counter variable with the two threads leads to error and using reentrant lock. Reentry lock makes it so that threads wait their turn instead of simultaneously. Locking code eliminates race conditions and enforces thread safety.
+
+CountDownLatch allows a parent thread to await worker threads
+
+Eventually locks were removed and not necessary when 
+using Atomic variable. Atomic varibles make threads wait 
+just like locks but are easier and cleaner to do.
